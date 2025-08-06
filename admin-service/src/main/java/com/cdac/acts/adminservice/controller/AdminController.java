f090cdb385b7f137c0ca82957e2af07dd0ff4548
Hemant1231 <hemantma121212@gmail.com>
2025-08-06 14:02:13 +0530

    Api for approve and reject

--- admin-service/src/main/java/com/cdac/acts/adminservice/controller/AdminController.java
+++ admin-service/src/main/java/com/cdac/acts/adminservice/controller/AdminController.java
@@ -49,6 +49,18 @@ public class AdminController {
         return ResponseEntity.ok(response);
     }
 
+    @PutMapping("/approve/{id}")
+    public ResponseEntity<String> approveKyc(@PathVariable("id") UUID userId) {
+        adminService.approveKyc(userId);
+        return ResponseEntity.ok("KYC approved successfully.");
+    }
+
+    @PutMapping("/reject/{id}")
+    public ResponseEntity<String> rejectKyc(@PathVariable("id") UUID userId) {
+        adminService.rejectKyc(userId);
+        return ResponseEntity.ok("KYC rejected and info deleted.");
+    }
+
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<Void> suspendUser(@PathVariable UUID id) {
